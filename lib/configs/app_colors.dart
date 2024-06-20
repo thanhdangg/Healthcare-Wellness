812831711c967aa516a9a8b983fb030e0b4b6b71
thanhdangg <102210310@sv1.dut.udn.vn>
2024-06-20 22:26:46 +0700

    chore: remove macos,  windows, web

--- lib/configs/app_colors.dart
+++ lib/configs/app_colors.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
-  
+  static const primaryColor = Color(0xFF0F67FE);
+  static const primaryLightColor = Color(0xFF458CFF);
+  static const primaryDarkColor = Color(0xFF003399);
+  static const backgroundColor = Color(0xFF242E49);
+  static const onErrorColor = Color(0xFFFA4D5E);
+
 }
