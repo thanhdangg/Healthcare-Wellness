b76c9a6539340b9321584a6d8f188f3c7be769b4
thanhdangg <102210310@sv1.dut.udn.vn>
2024-06-26 21:01:22 +0700

    UI First page

--- lib/configs/app_colors.dart
+++ lib/configs/app_colors.dart
@@ -6,5 +6,9 @@ class AppColors {
   static const primaryDarkColor = Color(0xFF003399);
   static const backgroundColor = Color(0xFF242E49);
   static const onErrorColor = Color(0xFFFA4D5E);
+  static const textLogoColor = Color(0xFF5D6A85);
+  static const backgroundColorFirstPage = Color(0xFF0F67FE);
+  static const backgroundColorFirstPageQuote = Color(0xFFD0E4FF);
+
 
 }
---
+++ lib/presentation/widgets/first_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare_wellness/configs/app_colors.dart';
+
+Widget buildPage(int index, double progressValue) {
+  switch (index) {
+    case 0:
+      return buildLogoFrame();
+    case 1:
+      return buildLoadingFrame(progressValue);
+    case 2:
+      return buildQuoteFrame();
+    case 3:
+      return buildLoadingNumberFrame(progressValue);
+    default:
+      return Container();
+  }
+}
+
+Widget buildLogoFrame() {
+  return const Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image(image: AssetImage('assets/images/monotone_health_plus.png')),
+        SizedBox(height: 20),
+        Image(image: AssetImage('assets/images/asklepios.png')),
+        SizedBox(height: 20),
+        Text(
+          'Your Intelligent AI Health & Wellness Companion.',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'PlusJakartaSans',
+            color: AppColors.textLogoColor,
+            wordSpacing: 2,
+            letterSpacing: 1,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget buildLoadingFrame(double progressValue) {
+  return Container(
+    decoration: const BoxDecoration(
+      color: AppColors.backgroundColorFirstPage,
+    ),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Loading',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 100, right: 100, bottom: 20, top: 10),
+            child: LinearProgressIndicator(
+              value: progressValue,
+              backgroundColor: const Color.fromARGB(82, 255, 255, 255),
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+              minHeight: 10,
+            ),
+            ),
+        ],    
+      ),
+    ),
+  );
+}
+
+Widget buildQuoteFrame() {
+  return Container(
+    decoration: const BoxDecoration(
+      color: AppColors.backgroundColorFirstPageQuote,
+    ),
+    child: const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage('assets/images/quote.png')),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+            child: Text(
+              '"Health is the complete harmony of the body and soul."',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: 'PlusJakartaSans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Text(
+            "— Aristotle",
+            style: TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.normal,
+                fontFamily: 'PlusJakartaSans',
+                color: Colors.blue),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildLoadingNumberFrame(double progressValue) {
+  return Container(
+    decoration: const BoxDecoration(
+      color: AppColors.backgroundColor,
+    ),
+    child: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          '${(progressValue * 100).toStringAsFixed(0)}%',
+          style: const TextStyle(
+            fontSize: 40,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    ),
+  );
+}
