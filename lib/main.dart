881cabe31f4dff703c832750120a308a05b4f739
thanhdangg <102210310@sv1.dut.udn.vn>
2024-07-01 01:23:57 +0700

    feat(welcome): marks app first launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare_wellness/screens/home/home_page.dart';
 import 'package:healthcare_wellness/screens/splash/splash_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<Widget> getLandingPage() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+
+    if (isFirstTime) {
+      prefs.setBool('isFirstTime', false);
+      return SplashPage(); 
+    } else {
+      return HomePage(title: "Home Page",); // Trả về HomePage nếu không phải lần đầu
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +28,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashPage(),
+      home: FutureBuilder<Widget>(
+        future: getLandingPage(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return snapshot.data!; 
+          } else {
+            return SplashPage(); 
+          }
+        },
+      ),
       debugShowCheckedModeBanner: false,      
     );
   }
