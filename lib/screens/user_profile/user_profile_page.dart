78bf9d24c81be2757afb07b001eeff9b83b3f604
Grace <luungoclan125690@gmail.com>
2024-08-28 23:11:55 +0700

    feat(nested-nav): add missing user profile page

---
+++ lib/screens/user_profile/user_profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class UserProfilePage extends StatefulWidget {
+  
+  const UserProfilePage({super.key});
+
+  @override
+  State<UserProfilePage> createState() => _UserProfilePageState();
+}
+
+class _UserProfilePageState extends State<UserProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('UserProfilePage'),
+      ),
+    );
+  }
+}
